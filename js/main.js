9e44886cc43adaaefbaa3896fc91a5bf8866f42b
Thiago Macedo <111804035+thgd86@users.noreply.github.com>
2023-10-12 20:22:59 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,15 +5,3 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
 })
-
-
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
-
-window.onscroll = function(){
-    if (window.pageYOffset >= header) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-        }
-    }
