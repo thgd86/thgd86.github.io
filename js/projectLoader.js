938f84169ad94838dc1a2c0187c14c3b5f0a2b59
Thiago Macedo <111804035+thgd86@users.noreply.github.com>
2023-10-14 01:14:57 -0300

    Add files via upload

---
+++ js/projectLoader.js
@@ -0,0 +1,24 @@
+const projects = [
+    {
+        title: "Em breve...",
+        description: "Descrição do projeto",
+        image: "images/template.png",
+        color: "gray",
+        link: "https://thgd86.github.io/"
+    }
+];
+
+const projectGrid = document.getElementById('project-grid');
+
+projects.forEach(project => {
+    const projectDiv = document.createElement('div');
+    projectDiv.classList.add('project');
+
+    projectDiv.innerHTML = `
+        <h2>${project.title}</h2>
+        <img src="${project.image}" alt="${project.title}">
+        <p class="project-description">${project.description}</p>
+        <a style="background-color: ${project.color};" href="${project.link}">Ver mais</a>
+    `;
+    projectGrid.appendChild(projectDiv);
+});
